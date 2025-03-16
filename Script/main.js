cc772ff799d9179e194014f70288497240d60d0d
navidiuming <navid.abolfathi@gmail.com>
2025-03-16 14:46:28 +0330

    add tasks , edit , and style

--- Script/main.js
+++ Script/main.js
@@ -1,54 +1,64 @@
-const container = document.querySelector('.container')
-const input = document.querySelector('.userInputTask')
-
-
-function newTask(name){
-    var self = this;
-    self.taskName = name;
-    self.iscompleted = false;
+function newTask(name) {
+  var self = this;
+  self.taskName = name;
+  self.iscompleted = false;
+  self.isEditing = false;
+  self.isActive = ko.observable(false);
+  self.isHide = ko.observable(true);
 }
-function AppViewModel(){
-    
-    
-    this.tasks = ko.observableArray([
-    ]);
-    this.addTask = function(){
-        let task = input.value;
-        task = task.trim();
-        if( task !=""){
 
-            let editBtn = document.createElement('button');
-            editBtn.innerHTML = "edit";
-            let deletBtn = document.createElement('button');
-            deletBtn.innerHTML = 'delet';
-            deletBtn.setAttribute("data-bind", "click:$root.deletTask");
-            let doneBtn = document.createElement('button');
-            doneBtn.innerHTML  = 'done';
-            let p = document.createElement('p');
-            p.innerHTML = task;
+function AppViewModel() {
+  var self = this;
+  self.tasks = ko.observableArray([]);
+  this.newTaskText = ko.observable("");
 
-            let todoItemDiv = document.createElement('div');
-            todoItemDiv.classList.add('todoItem');
-
-            todoItemDiv.appendChild(p);
-            todoItemDiv.appendChild(doneBtn);
-            todoItemDiv.appendChild(deletBtn);
-            todoItemDiv.appendChild(editBtn);
-
-            container.appendChild(todoItemDiv);
-
-            this.tasks.push( new newTask(task));
-        }
-        
-        console.log(this.tasks());
-        input.value = ""
+  self.loadTask = function () {
+    let savedTaskInLocalStorage = localStorage.getItem("tasks");
+    if (savedTaskInLocalStorage) {
+      let savedTaskInLocalStorageToParse = JSON.parse(savedTaskInLocalStorage);
+      this.tasks(
+        savedTaskInLocalStorageToParse.map((task) => {
+          let newTaskInstance = new newTask(task.taskName);
+          newTaskInstance.iscompleted = task.iscompleted;
+          newTaskInstance.isActive(task.iscompleted);
+          return newTaskInstance;
+        })
+      );
     }
+  };
 
-    this.deletTask = function(){
-        //this.tasks.remove();
-        console.log("hi");
+  this.loadTask();
+  self.addTask = function () {
+    let task = this.newTaskText().trim();
+    if (task != "") {
+      this.tasks.push(new newTask(task));
     }
-    
+    this.saveTask();
+    this.newTaskText("");
+  };
+
+  self.deletTask = function (task) {
+    this.tasks.remove(task);
+    this.saveTask();
+  };
+
+  self.saveTask = function () {
+    localStorage.setItem("tasks", ko.toJSON(this.tasks()));
+  };
+  this.done = function (task) {
+    console.log(task);
+    task.iscompleted = !task.iscompleted;
+    task.isActive(task.iscompleted);
+    this.saveTask();
+  };
 }
+
+self.edit = function (task) {
+    console.log(self);
+    console.log(this);
+    console.log("hi");
+    task.isHide(task.isEditing);
+    task.isEditing = !task.isEditing;
+};
 
 ko.applyBindings(new AppViewModel());
